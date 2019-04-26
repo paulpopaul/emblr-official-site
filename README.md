4f9a495582a09c994a52efaab7cf187cdc4a8184
Diego Ulloa <diegoulloao@icloud.com>
2019-04-26 03:58:06 -0400

    Go!! Go!! Goooo!!

---
+++ README.md
@@ -0,0 +1,40 @@
+# Ensambler®
+
+- **Desarrollo:** "Ensambler Official".
+- **Desarrolladores:**
+	- Diego Ulloa Ormeño.
+	- Paul Joseph Pineda.
+	- Álvaro Francisco Arriagada.
+- **Stack:** Wordpress, MySQL, PHP, SASS, HTML5, JS.
+- **Servicios:** Wordpress, MySQL, phpMyAdmin.
+- **Año:** 2019.
+
+## Instalación Proyecto en Entorno local.
+
+Abrir Terminal, navegar hasta directorio personal de Proyectos y ejecutar:
+
+```bash
+curl -L https://git.io/fjO2E | bash
+```
+
+--
+
+### Opcional:
+Añadir alias **"emblr-site"** al bash_profile para acceder directamente al directorio de trabajo.
+*(Reemplazar ".bash_profile" por el archivo bash indicado en caso de ser necesario).*
+
+En Terminal:
+```
+echo "alias 'emblr-site'='cd `pwd`'" >> ~/.bash_profile && source ~/.bash_profile
+```
+Uso:
+```
+emblr-site
+```
+
+--
+
+URL: http://www.ensambler.cl/
+--
+
+Ensambler® 2019. Ingeniería de Software para Soluciones Empresariales Integradas.
---
+++ wordpress/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'b4768dc698cad2262bd3098f8bcbb2ee25101b37');
+define( 'SECURE_AUTH_KEY',  'c72a4459c6fa71d40b6faa7ffbb0b92f24e5f97b');
+define( 'LOGGED_IN_KEY',    '68659de02ff70c142f093b280c5adce0d87c6214');
+define( 'NONCE_KEY',        '51586045acf7506ff0f2ef0726c8986b7840d2d2');
+define( 'AUTH_SALT',        'ca6fcb42f9343cd69ddfdf16d9905a0a9b3c279f');
+define( 'SECURE_AUTH_SALT', '593e481990c07d9be8e06c52f204588c61e3518a');
+define( 'LOGGED_IN_SALT',   '21a822352a2462e35570d59fae04a6f2db7f7fcb');
+define( 'NONCE_SALT',       '2c3803d31c11f2856419b25e21729a842a71ede7');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
