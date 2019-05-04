9ff223cfb92fafe9a596db5f617a41cbc4ee14cf
Diego Ulloa <diegoulloao@icloud.com>
2019-05-03 21:06:06 -0400

    Se sigue trabajando sobre redes responsive. Se traslada .home-social dentro de .home-content. Se logra alinear horizontalmente al medio. Se coporta igual en todos los dispositivos <= 1024px. Falta alinear verticalmente en el espacio disponible (desde botones hasta tope inferior).

--- wordpress/wp-content/themes/emblr-official/custom-pages/inicio.php
+++ wordpress/wp-content/themes/emblr-official/custom-pages/inicio.php
@@ -44,25 +44,26 @@
             </a>
         </div>
 
+        <ul class="home-social">
+            <li>
+                <a href="#0"><i class="fab fa-facebook-f" aria-hidden="true"></i><span>Facebook</span></a>
+            </li>
+            <li>
+                <a href="#0"><i class="fab fa-twitter" aria-hidden="true"></i><span>Twiiter</span></a>
+            </li>
+            <li>
+                <a href="#0"><i class="fab fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
+            </li>
+            <li>
+                <a href="#0"><i class="fab fa-behance" aria-hidden="true"></i><span>Behance</span></a>
+            </li>
+            <li>
+                <a href="#0"><i class="fab fa-dribbble" aria-hidden="true"></i><span>Dribbble</span></a>
+            </li>
+        </ul> <!-- end home-social -->
+
     </div> <!-- end home-content -->
 
-    <ul class="home-social">
-        <li>
-            <a href="#0"><i class="fab fa-facebook-f" aria-hidden="true"></i><span>Facebook</span></a>
-        </li>
-        <li>
-            <a href="#0"><i class="fab fa-twitter" aria-hidden="true"></i><span>Twiiter</span></a>
-        </li>
-        <li>
-            <a href="#0"><i class="fab fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
-        </li>
-        <li>
-            <a href="#0"><i class="fab fa-behance" aria-hidden="true"></i><span>Behance</span></a>
-        </li>
-        <li>
-            <a href="#0"><i class="fab fa-dribbble" aria-hidden="true"></i><span>Dribbble</span></a>
-        </li>
-    </ul> <!-- end home-social -->
 
     <div class="page-counter">01 / <span>05</span></div>
 
