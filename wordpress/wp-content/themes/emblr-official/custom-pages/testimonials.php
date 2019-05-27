057c77599f511bc7b3fd3bc3fd0e15dee48d9b89
AlvaroArriagada <alvaro@ensambler.cl>
2019-05-27 01:14:56 -0400

    Se agregaron margenes del testimonial, ademas se cargan comillas nuevas con un diseño mas grande

--- wordpress/wp-content/themes/emblr-official/custom-pages/testimonials.php
+++ wordpress/wp-content/themes/emblr-official/custom-pages/testimonials.php
@@ -12,10 +12,10 @@
             <div class="testimonials__slide">
                 <div class="foto-persona"> 
 
-                    <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente1.jpg"/></a>
+                    <a ><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente1.jpg"/></a>
                 </div>
-                <p>Qui ipsam temporibus quisquam vel. Maiores eos cumque distinctio nam accusantium ipsum. 
-                Laudantium quia consequatur molestias delectus culpa facere hic dolores aperiam. Accusantium quos qui praesentium corposri.</p>
+                <p>"Qui ipsam temporibus quisquam vel. Maiores eos cumque distinctio nam accusantium ipsum. 
+                Laudantium quia consequatur molestias delectus culpa facere hic dolores aperiam. Accusantium quos qui praesentium corposri."</p>
                 <div class="testimonials__author">
                     Claudia Alvarado
                     <span>Directora de Microsoft</span>
@@ -24,7 +24,7 @@
 
             <div class="testimonials__slide">
                 <div class="foto-persona"> 
-                    <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente2.jpg"/></a>
+                    <a ><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente2.jpg"/></a>
                 </div>
                 <p>Excepturi nam cupiditate culpa doloremque deleniti repellat. Veniam quos repellat voluptas animi adipisci.
                 Nisi eaque consequatur. Quasi voluptas eius distinctio. Atque eos maxime. Qui ipsam temporibus quisquam vel.</p>
@@ -37,7 +37,7 @@
             <div class="testimonials__slide">
                 <div class="foto-persona"> 
 
-                    <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente1.jpg"/></a>
+                    <a ><img src="<?php echo get_template_directory_uri();?>/images/clientes/cliente1.jpg"/></a>
                 </div>
                 <p>Repellat dignissimos libero. Qui sed at corrupti expedita voluptas odit. Nihil ea quia nesciunt. Ducimus aut sed ipsam.  
                 Autem eaque officia cum exercitationem sunt voluptatum accusamus. Quasi voluptas eius distinctio.</p>
