fc428e270164a2a7b587842f277c78265a591e57
Diego Ulloa <diegoulloao@icloud.com>
2019-05-29 03:12:05 -0400

    Se arregla problema de carga web por no incluír archivos requeridos por wordpress.

---
+++ wordpress/wp-content/themes/emblr-official/sub-sections/lo-que-dicen-nuestros-clientes.php
@@ -0,0 +1,16 @@
+<!-- sub-section: clientes logos
+================================================== -->
+
+<section id="testimonials-clientes-logo" class="section-page">
+	<div class="clientes-logo">
+		<img src="<?= get_template_directory_uri() ?>/images/clientes/logos/logocliente-01.svg" alt="">
+	</div>
+
+	<div class="clientes-logo">
+		<img src="<?= get_template_directory_uri() ?>/images/clientes/logos/logocliente-02.svg" alt="">
+	</div>
+
+	<div class="clientes-logo">
+		<img src="<?= get_template_directory_uri() ?>/images/clientes/logos/logocliente-03.svg" alt="">
+	</div>
+</section>
