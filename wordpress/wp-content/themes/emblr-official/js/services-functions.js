811d81ea7774f919fd09fa620f80a68be603bba9
Alvaro <alvaro@ensambler.cl>
2019-07-08 23:33:19 -0400

    Se agregan los 15 servicios, se agrega la flecha en cada uno de los rectangulos, además de estructurar el archivo php servicios.php, queda pendiente animar cada transición nota: Echar vistazo a margenes y tamaños de textos

--- wordpress/wp-content/themes/emblr-official/js/services-functions.js
+++ wordpress/wp-content/themes/emblr-official/js/services-functions.js
@@ -1,43 +1,44 @@
 $(document).ready(function(){
-	$(".servicio-minimizadof1").hide();
-	$(".servicio-minimizadof13").hide();
-    /*$("#min-2").hide();
-    $("#min-3").hide();*/
-    $("#servicio-4").hide();
-    $("#servicio-5").hide();
-    $("#servicio-6").hide();
-    $("#servicio-7").hide();
-    $("#servicio-8").hide();
-    $("#servicio-9").hide();
+	$("#fila1 .servicio-minimizado").hide();
+    $("#fila2-servicio").hide();
+    $("#fila3-servicio").hide();
+    $("#fila4-servicio").hide();
+    $("#fila5-servicio").hide();
 
+$("#fila1").click(function(){
+    $(".servicio-minimizado").show();
+    $("#fila1 .servicio-minimizado").hide();
+    $(".descripcion").hide();
+    $("#fila1-servicio").slideDown();  
+    }); 
 
-$(".servicio-minimizadof2").click(function(){
-	$(".servicio-minimizadof2").hide();
-	$(".servicio-minimizadof23").hide();
-	$(".servicio-minimizadof1").show();
-	$(".servicio-minimizadof13").show();
-	$("#servicio-1").hide();
-    $("#servicio-2").hide();
-    $("#servicio-3").hide();
-    $("#servicio-4").slideDown(800);
-    $("#servicio-5").slideDown(800);
-    $("#servicio-6").slideDown(800);
+$("#fila2").click(function(){
+    $(".servicio-minimizado").show();
+    $("#fila2 .servicio-minimizado").hide();
+    $(".descripcion").hide();
+    $("#fila2-servicio").show();  
+    }); 
 
-	});	
+$("#fila3").click(function(){
+    $(".servicio-minimizado").show();
+    $("#fila3 .servicio-minimizado").hide();
+    $(".descripcion").hide();
+    $("#fila3-servicio").show(); 
+    }); 
 
-$(".servicio-minimizadof1").click(function(){
-	$(".servicio-minimizadof1").hide();
-	$(".servicio-minimizadof13").hide();
-	$(".servicio-minimizadof2").show();
-	$(".servicio-minimizadof23").show();
-	$("#servicio-4").hide();
-    $("#servicio-5").hide();
-    $("#servicio-6").hide();
-    $("#servicio-3").slideDown();
-	$("#servicio-1").slideDown();
-    $("#servicio-2").slideDown();
+$("#fila4").click(function(){
+    $(".servicio-minimizado").show();
+    $("#fila4 .servicio-minimizado").hide();
+    $(".descripcion").hide();
+    $("#fila4-servicio").show();  
+    }); 
 
-	});	
+$("#fila5").click(function(){
+    $(".servicio-minimizado").show();
+    $("#fila5 .servicio-minimizado").hide();
+    $(".descripcion").hide();
+    $("#fila5-servicio").show();  
+    }); 
 
 });	
 
