83d1dc61032f01416d198c7fd381ee5ea1b57d3e
Diego Ulloa <diegoulloao@icloud.com>
2019-07-15 11:32:10 -0400

    Se registra menú principal y se corrige problema de mostrar redes sociales en todos los posts y configuraciones.

--- wordpress/wp-content/themes/emblr-official/functions.php
+++ wordpress/wp-content/themes/emblr-official/functions.php
@@ -1,6 +1,10 @@
 <?php
 
-	if ( function_exists('acf_add_options_page') ) {
+	if ( function_exists("register_nav_menu") )
+		register_nav_menu( "menu-principal", "Menú principal" );
+
+
+	if ( function_exists("acf_add_options_page") ) {
 
 		acf_add_options_page(array(
 			'page_title' => "Ensambler · Opciones del tema",
@@ -11,26 +15,26 @@
 
 
 		acf_add_options_sub_page(array(
-			'page_title' => 'Ajustes del tema: @General',
-			'menu_title' => 'General',
+			'page_title' => "Ajustes del tema: @General",
+			'menu_title' => "General",
 			'menu_slug' => "ensambler-options-general",
-			'parent_slug' => 'ensambler-options'
+			'parent_slug' => "ensambler-options"
 		));
 
 
 		acf_add_options_sub_page(array(
-			'page_title' => 'Ajustes del tema: @Inicio',
-			'menu_title' => 'Inicio',
+			'page_title' => "Ajustes del tema: @Inicio",
+			'menu_title' => "Inicio",
 			'menu_slug' => "ensambler-options-inicio",
-			'parent_slug' => 'ensambler-options'
+			'parent_slug' => "ensambler-options"
 		));
 
 
 		acf_add_options_sub_page(array(
-			'page_title' => 'Ajustes del tema: @Footer',
-			'menu_title' => 'Footer',
+			'page_title' => "Ajustes del tema: @Footer",
+			'menu_title' => "Footer",
 			'menu_slug' => "ensambler-options-footer",
-			'parent_slug' => 'ensambler-options'
+			'parent_slug' => "ensambler-options"
 		));
 
 	}
