e2897bc001af914778dbc6f7473cdf92a9a36075
Alvaro <alvaro@ensambler.cl>
2019-07-15 22:22:50 -0400

    Se modifica la estructura de servicios.php, estilos asociados, además se arreglan las imagenes SVG para que cubran el fondo completo

--- wordpress/wp-content/themes/emblr-official/custom-pages/servicios.php
+++ wordpress/wp-content/themes/emblr-official/custom-pages/servicios.php
@@ -4,79 +4,72 @@
     <h1 data-aos="fade-up"> Servicios </h1>
     <p class="aos-init aos-animate" data-aos="fade-up" > Trabajamos contigo, no para ti</p>
 
-    <div class="row services" data-aos="fade-up">
-            <div class="servicio-izquierda">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Desarrollo de sitios web</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura. Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+    <div class="row-services" data-aos="fade-up">
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Desarrollo de sitios web</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura. Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-medio">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Aplicaciones y plataforma web</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Aplicaciones y plataforma web</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-derecha">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Diseño web (UI/UX)</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Diseño web (UI/UX)</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-izquierda">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Sistemas de venta (POS)</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+     
+    </div>
+
+    <div class="row-services" data-aos="fade-up">
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Desarrollo de sitios web</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura. Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.Ensambler cree en tus sueños, y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-medio">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Aplicaciones de administración (CRUD)</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Aplicaciones y plataforma web</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-derecha">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Servicios y configuración de servidores</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+            <div class="box">
+                <div class="box-container">
+                    <div class="box-content">
+                        <i class="fas fa-desktop"></i>
+                        <h1><span>Diseño web (UI/UX)</span></h1>
+                        <hr/>
+                        <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
+                    </div>
                 </div>
             </div>
-            <div class="servicio-izquierda">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Posicionamiento web (SEO)</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
-                </div>
-            </div>
-            <div class="servicio-medio">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Analítica y estadísticas para sitios web.</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
-                </div>
-            </div>
-            <div class="servicio-derecha">
-                <div class="texto-servicio">
-                    <i class="fas fa-desktop"></i>
-                    <h1><span>Tiendas online (E-commerce)</span></h1>
-                    <hr/>
-                    <p><strong>Ensambler cree en tus sueños y es por eso que queremos brindarte la mejor asesoría e infraestructura.</strong></p>
-                </div>
-            </div>
+     
     </div>
 
     
