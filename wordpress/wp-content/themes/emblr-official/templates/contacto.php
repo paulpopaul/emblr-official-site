29c2a94d5ad836f7c6baec3ac15b3aaae44a7356
Paul <hallo,paul@icloud.com>
2019-11-04 19:10:25 -0300

    responsive de contacto completo + responsive team parcialmente solucionado, problema al agregar nueva caja

--- wordpress/wp-content/themes/emblr-official/templates/contacto.php
+++ wordpress/wp-content/themes/emblr-official/templates/contacto.php
@@ -38,11 +38,11 @@
   <div class="contact-bg">
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 900 720" enable-background="new 0 0 900 720" xml:space="preserve" id="worldmap"></svg>
   </div>
-  
-  <div class="contact-content">
+
     <h1 data-aos="fade-up"> <? the_title() ?> </h1>
     <p data-aos="fade-up"> <?= get_field( 'subtitulo', $post->ID ) ?> </p>
-
+  
+    <div class="contact-content">
     <!-- <img src="<?= get_template_directory_uri() ?>/images/logo.svg" alt="" data-aos="fade-up"> -->
 
     <div class="terminal" data-aos="fade-up">
@@ -61,8 +61,29 @@
             <span id="prompt-field">></span>
             <input id="prompt" type="text" autocomplete="off">
           </div>
-
         </div>
-    </div>
+    </div> <!-- terminal -->
+
+    <form class="traditional-form" method="post" action="">
+        <div>
+            <pre>Escribe tu nombre</pre>
+            <input type="text" name="nombre" id="" class="nombre" autofocus>
+        </div>
+
+        <div>
+            <pre>Escribe tu correo</pre>
+            <input type="text" name="email" id="" class="email">
+        </div>
+
+        <div>
+            <pre>Consulta</pre>
+            <textarea name="subject" id="" class="subject"></textarea>
+        </div>
+
+        <div>
+            <pre>Presiona <span>enviar</span> para confirmar el mensaje</pre>
+            <input type="submit" value="enviar" class="btn btn--primary btn--large">
+        </div>
+    </form> <!-- traditional-form -->
   </div>
 </section> <!-- end s-contact -->
--- wordpress/wp-content/themes/emblr-official/templates/team.php
+++ wordpress/wp-content/themes/emblr-official/templates/team.php
@@ -33,11 +33,10 @@
 
 
 
-<!-- team
-================================================== -->
+<!-- team ================================================== -->
 <section class="team-section target-section section-page">
-        <h1 data-aos="fade-up" class="aos-init aos-animate"> Conoce el Team </h1>
-        <div class="slick-team-container">
+    <h1 data-aos="fade-up" class="aos-init aos-animate"> Conoce el Team </h1>
+    <div class="slick-team-container">
         <!-- prev, next arrows -->
         <span class="slick-team-prev slick-team-arrow">
             <i class="fas fa-caret-left fa-2x"></i>
@@ -47,81 +46,104 @@
             <i class="fas fa-caret-right fa-2x"></i>
         </span>
 
-            <div class="container-team">
-               <!--  <div class="row"> -->
-                    <div class="box-team">
-                        <div class="our-team">
-                            <div class="pic">
-                                <img src="https://psicologiaymente.com/media/xy/pq/jr/xypqjrQ4qG/xavier-molina/xavier-molina-medium.jpg">
-                            </div>
-                            <div class="team-content">
-                                <h3 class="title">Alvaro Arriagada</h3>
-                                <span class="post">Desarrollador Front-End</span>
-                                    <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
-                            </div>
-                            <ul class="social">
-                                <li>
-                                    <a href="#" target="_blank" class="fab fa-github"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-instagram"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-linkedin"></a>
-                                </li>
-                            </ul>
-                        </div>
+        <div class="container-team">
+            <div class="box-team">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://psicologiaymente.com/media/xy/pq/jr/xypqjrQ4qG/xavier-molina/xavier-molina-medium.jpg">
                     </div>
+                    <div class="team-content">
+                        <h3 class="title">Alvaro Arriagada</h3>
+                        <span class="post">Desarrollador Front-End</span>
+                            <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
+                    </div>
+                    <ul class="social">
+                        <li>
+                            <a href="#" target="_blank" class="fab fa-github"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-linkedin"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
-                     <div class="box-team">
-                        <div class="our-team">
-                            <div class="pic">
-                                <img src="https://psicologiaymente.com/media/pn/mq/dz/pnmqdzGNYE/arturo-torres/arturo-torres-medium.jpg">
-                            </div>
-                            <div class="team-content">
-                                <h3 class="title">Diego Ulloa</h3>
-                                <span class="post">Desarrollador back-end</span>
-                                    <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
-                            </div>
-                            <ul class="social">
-                                <li>
-                                    <a href="#" target="_blank" class="fab fa-github"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-instagram"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-linkedin"></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+            <div class="box-team">
+                <div class="our-team">
+                <div class="pic">
+                    <img src="https://psicologiaymente.com/media/pn/mq/dz/pnmqdzGNYE/arturo-torres/arturo-torres-medium.jpg">
+                </div>
+                <div class="team-content">
+                    <h3 class="title">Diego Ulloa</h3>
+                    <span class="post">Desarrollador back-end</span>
+                        <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
+                </div>
+                <ul class="social">
+                    <li>
+                        <a href="#" target="_blank" class="fab fa-github"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fab fa-instagram"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fab fa-linkedin"></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
 
-                    <div class="box-team">
-                        <div class="our-team">
-                            <div class="pic">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiVkcxtk6Zeg7pnbnvq5X4tThpehvGp82qgd0jOCe9iZz2Fnldlg">
-                            </div>
-                            <div class="team-content">
-                                <h3 class="title">Paul Pineda</h3>
-                                <span class="post">Diseñador (UI/UX)</span>
-                                    <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
-                            </div>
-                            <ul class="social">
-                                <li>
-                                    <a href="#" target="_blank" class="fab fa-github"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-instagram"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fab fa-linkedin"></a>
-                                </li>
-                            </ul>
-                        </div>
+            <div class="box-team">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiVkcxtk6Zeg7pnbnvq5X4tThpehvGp82qgd0jOCe9iZz2Fnldlg">
                     </div>
-<!-- 
-                </div> -->
-            </div> 
+                    <div class="team-content">
+                        <h3 class="title">Paul Pineda</h3>
+                        <span class="post">Diseñador (UI/UX)</span>
+                            <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
+                    </div>
+                    <ul class="social">
+                        <li>
+                            <a href="#" target="_blank" class="fab fa-github"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-linkedin"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="box-team">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiVkcxtk6Zeg7pnbnvq5X4tThpehvGp82qgd0jOCe9iZz2Fnldlg">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Paul Pineda</h3>
+                        <span class="post">Diseñador (UI/UX)</span>
+                        <p> Ensambler cree en tus sueños, y es por eso que queremos brundarte la mejor asesoría e infrastructura del mercado, aquellos que te permitan hacerlo realidad.</p>
+                    </div>
+                    <ul class="social">
+                        <li>
+                            <a href="#" target="_blank" class="fab fa-github"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </li>
+                        <li>
+                            <a href="#" class="fab fa-linkedin"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
     </div>
 </section>
