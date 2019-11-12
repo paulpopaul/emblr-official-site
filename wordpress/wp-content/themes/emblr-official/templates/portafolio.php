0ccf1fdf052480a215b3983ff7b5eca6ccfd4acb
Diego Ulloa <diegoulloao@icloud.com>
2019-11-12 19:48:52 -0300

    Se arregla problema pestañeo en portafolio (data-aos)

--- wordpress/wp-content/themes/emblr-official/templates/portafolio.php
+++ wordpress/wp-content/themes/emblr-official/templates/portafolio.php
@@ -54,214 +54,254 @@ global $post;
 
             <div id="gallery">
 
-                    <div class="masonry__brick item-folio gallery-item logo " href="#" data-cat="logo" data-aos="fade-up">
-                        <div class="item-folio__thumb">
-                            <a href="https://unsplash.it/1000/1000?image=1075" class="thumb-link" data-size="1000x1000">
-                                <img src="https://unsplash.it/1000/1000?image=1075" alt="" />
-                            </a>
-                        </div>
-                        <div class="item-folio__text">
-                            <h3 class="item-folio__title">
-                                Shutterbug
-                            </h3>
-                            <p class="item-folio__cat">
-                                Branding
-                            </p>
-                        </div>
-                        <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
-                            <i class="icon-link"></i>
+                <div class="masonry__brick item-folio gallery-item logo " href="#" data-cat="logo">
+                    <div class="item-folio__thumb">
+                        <a href="https://unsplash.it/1000/1000?image=1075" class="thumb-link" data-size="1000x1000">
+                            <img src="https://unsplash.it/1000/1000?image=1075" alt="" />
                         </a>
-                        <div class="item-folio__caption">
-                            <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
-                        </div>
-                    </div> <!-- end masonry__brick -->
+                    </div>
+
+                    <div class="item-folio__text">
+                        <h3 class="item-folio__title">
+                            Shutterbug
+                        </h3>
+
+                        <p class="item-folio__cat">
+                            Branding
+                        </p>
+                    </div>
+
+                    <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
+                        <i class="icon-link"></i>
+                    </a>
+
+                    <div class="item-folio__caption">
+                        <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    </div>
+                </div> <!-- end masonry__brick -->
 
 
-                    <div class="masonry__brick item-folio gallery-item logo " href="#" data-cat="logo" data-aos="fade-up">
-                        <div class="item-folio__thumb">
-                            <a href="https://unsplash.it/1000/1000?image=1084" class="thumb-link" data-size="1000x1000">
-                                <img src="https://unsplash.it/1000/1000?image=1083" alt="" />
-                            </a>
-                        </div>
-                        <div class="item-folio__text">
-                            <h3 class="item-folio__title">
-                                Shutterbug
-                            </h3>
-                            <p class="item-folio__cat">
-                                Branding
-                            </p>
-                        </div>
-                        <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
-                            <i class="icon-link"></i>
+                <div class="masonry__brick item-folio gallery-item logo " href="#" data-cat="logo">
+                    <div class="item-folio__thumb">
+                        <a href="https://unsplash.it/1000/1000?image=1084" class="thumb-link" data-size="1000x1000">
+                            <img src="https://unsplash.it/1000/1000?image=1083" alt="" />
                         </a>
-                        <div class="item-folio__caption">
-                            <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
-                        </div>
-                    </div> <!-- end masonry__brick -->
+                    </div>
+
+                    <div class="item-folio__text">
+                        <h3 class="item-folio__title">
+                            Shutterbug
+                        </h3>
+
+                        <p class="item-folio__cat">
+                            Branding
+                        </p>
+                    </div>
+
+                    <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
+                        <i class="icon-link"></i>
+                    </a>
+
+                    <div class="item-folio__caption">
+                        <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    </div>
+                </div> <!-- end masonry__brick -->
 
 
-                    <div class="masonry__brick item-folio gallery-item web " href="#" data-cat="web" data-aos="fade-up">
-                        <div class="item-folio__thumb">
-                            <a href="https://unsplash.it/1000/1000?image=1082" class="thumb-link" data-size="1000x1000">
-                                <img src="https://unsplash.it/1000/1000?image=1082" alt="" />
-                            </a>
-                        </div>
-                        <div class="item-folio__text">
-                            <h3 class="item-folio__title">
-                                Shutterbug
-                            </h3>
-                            <p class="item-folio__cat">
-                                Branding
-                            </p>
-                        </div>
-                        <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
-                            <i class="icon-link"></i>
+                <div class="masonry__brick item-folio gallery-item web " href="#" data-cat="web">
+                    <div class="item-folio__thumb">
+                        <a href="https://unsplash.it/1000/1000?image=1082" class="thumb-link" data-size="1000x1000">
+                            <img src="https://unsplash.it/1000/1000?image=1082" alt="" />
                         </a>
-                        <div class="item-folio__caption">
-                            <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
-                        </div>
-                    </div> <!-- end masonry__brick -->
+                    </div>
 
-                    <div class="masonry__brick item-folio gallery-item print " href="#" data-cat="print" data-aos="fade-up">
-                        <div class="item-folio__thumb">
-                            <a href="https://unsplash.it/1000/1000?image=1084" class="thumb-link" data-size="1000x1000">
-                                <img src="https://unsplash.it/1000/1000?image=1081" alt="" />
-                            </a>
-                        </div>
-                        <div class="item-folio__text">
-                            <h3 class="item-folio__title">
-                                Shutterbug
-                            </h3>
-                            <p class="item-folio__cat">
-                                Branding
-                            </p>
-                        </div>
-                        <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
-                            <i class="icon-link"></i>
+                    <div class="item-folio__text">
+                        <h3 class="item-folio__title">
+                            Shutterbug
+                        </h3>
+
+                        <p class="item-folio__cat">
+                            Branding
+                        </p>
+                    </div>
+
+                    <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
+                        <i class="icon-link"></i>
+                    </a>
+
+                    <div class="item-folio__caption">
+                        <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    </div>
+                </div> <!-- end masonry__brick -->
+
+
+                <div class="masonry__brick item-folio gallery-item print " href="#" data-cat="print">
+                    <div class="item-folio__thumb">
+                        <a href="https://unsplash.it/1000/1000?image=1084" class="thumb-link" data-size="1000x1000">
+                            <img src="https://unsplash.it/1000/1000?image=1081" alt="" />
                         </a>
-                        <div class="item-folio__caption">
-                            <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
-                        </div>
-                    </div> <!-- end masonry__brick -->
+                    </div>
 
-                    <div class="masonry__brick item-folio gallery-item strategy " href="#" data-cat="strategy" data-aos="fade-up">
-                        <div class="item-folio__thumb">
-                            <a href="https://unsplash.it/1000/1000?image=1080" class="thumb-link" data-size="1000x1000">
-                                <img src="https://unsplash.it/1000/1000?image=1080" alt="" />
-                            </a>
-                        </div>
-                        <div class="item-folio__text">
-                            <h3 class="item-folio__title">
-                                Shutterbug
-                            </h3>
-                            <p class="item-folio__cat">
-                                Branding
-                            </p>
-                        </div>
-                        <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
-                            <i class="icon-link"></i>
+                    <div class="item-folio__text">
+                        <h3 class="item-folio__title">
+                            Shutterbug
+                        </h3>
+
+                        <p class="item-folio__cat">
+                            Branding
+                        </p>
+                    </div>
+
+                    <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
+                        <i class="icon-link"></i>
+                    </a>
+
+                    <div class="item-folio__caption">
+                        <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    </div>
+                </div> <!-- end masonry__brick -->
+
+
+                <div class="masonry__brick item-folio gallery-item strategy " href="#" data-cat="strategy">
+                    <div class="item-folio__thumb">
+                        <a href="https://unsplash.it/1000/1000?image=1080" class="thumb-link" data-size="1000x1000">
+                            <img src="https://unsplash.it/1000/1000?image=1080" alt="" />
                         </a>
-                        <div class="item-folio__caption">
-                            <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
-                        </div>
-                    </div> <!-- end masonry__brick -->
+                    </div>
 
-                <div class="masonry__brick item-folio gallery-item print " href="#" data-cat="print" data-aos="fade-up">
+                    <div class="item-folio__text">
+                        <h3 class="item-folio__title">
+                            Shutterbug
+                        </h3>
+
+                        <p class="item-folio__cat">
+                            Branding
+                        </p>
+                    </div>
+
+                    <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
+                        <i class="icon-link"></i>
+                    </a>
+
+                    <div class="item-folio__caption">
+                        <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
+                    </div>
+                </div> <!-- end masonry__brick -->
+
+
+                <div class="masonry__brick item-folio gallery-item print " href="#" data-cat="print">
                     <div class="item-folio__thumb">
                         <a href="https://unsplash.it/1000/1000?image=1079" class="thumb-link" data-size="1000x1000">
                             <img src="https://unsplash.it/1000/1000?image=1079" alt="" />
                         </a>
                     </div>
+
                     <div class="item-folio__text">
                         <h3 class="item-folio__title">
                             Shutterbug
                         </h3>
+
                         <p class="item-folio__cat">
                             Branding
                         </p>
                     </div>
+
                     <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
                         <i class="icon-link"></i>
                     </a>
+
                     <div class="item-folio__caption">
                         <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
                     </div>
                 </div> <!-- end masonry__brick -->
 
-                <div class="masonry__brick item-folio gallery-item web " href="#" data-cat="web" data-aos="fade-up">
+
+                <div class="masonry__brick item-folio gallery-item web " href="#" data-cat="web">
                     <div class="item-folio__thumb">
                         <a href="https://unsplash.it/1000/1000?image=1078" class="thumb-link" data-size="1000x1000">
                             <img src="https://unsplash.it/1000/1000?image=1078" alt="" />
                         </a>
                     </div>
+
                     <div class="item-folio__text">
                         <h3 class="item-folio__title">
                             Shutterbug
                         </h3>
+
                         <p class="item-folio__cat">
                             Branding
                         </p>
                     </div>
+
                     <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
                         <i class="icon-link"></i>
                     </a>
+
                     <div class="item-folio__caption">
                         <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
                     </div>
                 </div> <!-- end masonry__brick -->
 
 
-                <div class="masonry__brick item-folio gallery-item strategy " href="#" data-cat="strategy" data-aos="fade-up">
+                <div class="masonry__brick item-folio gallery-item strategy " href="#" data-cat="strategy">
                     <div class="item-folio__thumb">
                         <a href="https://unsplash.it/1000/1000?image=1077" class="thumb-link" data-size="1000x1000">
                             <img src="https://unsplash.it/1000/1000?image=1077" alt="" />
                         </a>
                     </div>
+
                     <div class="item-folio__text">
                         <h3 class="item-folio__title">
                             Shutterbug
                         </h3>
+
                         <p class="item-folio__cat">
                             Branding
                         </p>
                     </div>
+
                     <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
                         <i class="icon-link"></i>
                     </a>
+
                     <div class="item-folio__caption">
                         <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
                     </div>
                 </div> <!-- end masonry__brick -->
 
 
-                <div class="masonry__brick item-folio gallery-item web strategy" href="#" data-cat="web strategy" data-aos="fade-up">
+                <div class="masonry__brick item-folio gallery-item web strategy" href="#" data-cat="web strategy">
                     <div class="item-folio__thumb">
                         <a href="https://unsplash.it/1000/1000?image=1071" class="thumb-link" data-size="1000x1000">
                             <img src="https://unsplash.it/1000/1000?image=1071" alt="" />
                         </a>
                     </div>
+
                     <div class="item-folio__text">
                         <h3 class="item-folio__title">
                             Shutterbug
                         </h3>
+
                         <p class="item-folio__cat">
                             Branding
                         </p>
                     </div>
+
                     <a href="https://www.behance.net/" class="item-folio__project-link" title="Project link">
                         <i class="icon-link"></i>
                     </a>
+
                     <div class="item-folio__caption">
                         <p>Vero molestiae sed aut natus excepturi. Et tempora numquam. Temporibus iusto quo.Unde dolorem corrupti neque nisi.</p>
                     </div>
                 </div> <!-- end masonry__brick -->
 
-
             </div><!--/gallery-->
 
         </div><!--/gallery-wrap-->
 
-    </div>
+    </div> <!-- /wrap -->
+
 </section>
 
 
