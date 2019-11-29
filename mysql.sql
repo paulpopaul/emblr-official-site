34e302c61eb31387472f08e8704c56714eb80107
Alvaro <whitesnake@MacBook-Pro-de-Alvaro.local>
2019-11-29 09:40:07 -0300

    Se agrega base de datos actualizada: sobre-nosotros

--- mysql.sql
+++ mysql.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: wordpress
 -- ------------------------------------------------------
--- Server version	5.7.25
+-- Server version	5.7.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -408,4 +408,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-16  2:01:47
+-- Dump completed on 2019-11-29 12:39:53
