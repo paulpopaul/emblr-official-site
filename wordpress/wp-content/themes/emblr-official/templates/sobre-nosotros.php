3a150ad1dce3c6077156e4f781660af969309eb6
Alvaro <whitesnake@MacBook-Pro-de-Alvaro.local>
2019-12-03 19:59:19 -0300

    Se agregan los nuevos iconos, fondo negro al ser seleccionado, se cambia el grosor de las lineas, también para el circulo del centro, se agrega animación al hacer click, se agranda el circulo de 9x9, se agrega botón ver equipo y se distribuye ambos divs principales en una relación de 45%/55%

--- wordpress/wp-content/themes/emblr-official/templates/sobre-nosotros.php
+++ wordpress/wp-content/themes/emblr-official/templates/sobre-nosotros.php
@@ -41,14 +41,16 @@
     <!-- end section-header -->
 
     <div class="features-content">
-        <div data-features-tabs="" class="features-content-col">
+        <div data-features-tabs class="features-content-col" id="text-block">
             <div id="feature-1" class="features-textblock animated fadeIn __active">
                 <p> ¿Quiénes sómos? </p>
                 <p> Sómos una empresa de desarrollo de proyectos informáticos con más de 5 años de experiencia en el área de Innovación y Desarrollo.
 
 Nuestros conocimientos y experiencias con las tecnologías actuales del mercado, son el pilar fundamental de nuestra organización.
 </p>
+            <input class="view-team btn btn--primary btn--large" id="view-team" type="button" value="ver equipo">
             </div>
+
             <div id="feature-2" class="features-textblock animated fadeIn">
                 <p>Misión</p>
                 <p> Apasionados por abordar las problemáticas de nuestros clientes, buscamos entregar la mejor solución acorde a su presupuesto.
@@ -61,20 +63,45 @@ Nuestra lema “Ingeniería de software para Soluciones Empresariales Integradas
             <div id="feature-3" class="features-textblock animated fadeIn">
                 <p>Visión</p>
                 <p> La tecnología en la actualidad es una herramienta imprescindible para las empresas e instituciones que buscan crecer y producir de forma eficaz. Buscamos ser un referente en el mercado nacional en áreas de las tecnologías de información, por esto es que hemos decidido ser una empresa en constante innovación, capaz de solucionar cualquier problemática que el sector de la tecnología requiera.
-</p>
+                </p>
             </div>
+             
         </div>
-        <div class="features-content-col">
+
+        <div id="figure-block" class="features-content-col">
             <div data-features-nav="" class="features-graph">
-                <div class="button-holder"><a href="#feature-1" class="fas fa-users btn-with-icon __active"></a><a href="#feature-2" class="fas fa-bullseye btn-with-icon"></a><a href="#feature-3" class="far fa-eye btn-with-icon"></a></div>
+                <div class="top-features">
+                    <div class="button-holder">
+                        <a href="#feature-1" class="fas fa-users btn-with-icon __active"></a>
+                        <a href="#feature-2" class="fas fa-bullseye btn-with-icon"></a>
+                        <a href="#feature-3" class="far fa-eye btn-with-icon"></a>
+                    </div>
+                    <div class="tagAboutUs">
+                        <p id="tagNosotros"> Nosotros </p>
+                        <p id="tagMision"> Misión </p>
+                        <p id="tagVision"> Visión </p>
+                    </div>
+                </div>
                 <div class="animation-holder">
                     <span class="flash-oval">
-                <img src="http://127.0.0.1:8080/wp-content/themes/emblr-official/images/logo.svg" alt="pulse">
-                </span>
+                        <img src="http://127.0.0.1:8080/wp-content/themes/emblr-official/images/logo.svg" alt="pulse">
+                        <div class="wave hidden wave-anim"></div>
+                           <div class="wave2 hidden wave-anim"></div>
+                           <div class="wave3 hidden wave-anim"></div>
+                           <div class="wave4 hidden wave-anim"></div>
+                    </span>
                 </div>
+
                 <div class="button-holder">
-                    <a href="#feature-4" class="fas fa-quote-left btn-with-icon">
-                        </a><a href="#feature-5" class="fas fa-check btn-with-icon"></a><a href="#feature-6" class="far fa-registered btn-with-icon"></a></div>
+                    <a href="#feature-4" class="fas fa-quote-left btn-with-icon"></a>
+                    <a href="#feature-5" class="fas fa-check btn-with-icon"></a>
+                    <a href="#feature-6" class="far fa-registered btn-with-icon"></a>
+                </div>
+                <div class="tagAboutUs">
+                    <p id="tagFundacion"> Fundación </p>
+                    <p id="tagFeatures"> Features </p>
+                    <p id="tagDerecho"> Derecho </p>
+                </div>
             </div>
         </div>
     </div>
