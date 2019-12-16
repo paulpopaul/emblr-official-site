7512f322822b3f919e0aa9c4dfe8b50ff4572777
Paul Joseph Pineda <pauljosephpineda@MacBook-Pro-de-Paul.local>
2019-12-16 17:47:04 -0300

    merge de aboutus sobre dev y solucion de conflicto

--- wordpress/wp-content/themes/emblr-official/templates/sobre-nosotros.php
+++ wordpress/wp-content/themes/emblr-official/templates/sobre-nosotros.php
@@ -41,14 +41,16 @@
     <!-- end section-header -->
 
     <div class="features-content">
-        <div data-features-tabs="" class="features-content-col">
+        <div data-features-tabs class="features-content-col" id="text-block">
             <div id="feature-1" class="features-textblock animated fadeIn __active">
                 <p> ¿Quiénes sómos? </p>
                 <p> Sómos una empresa de desarrollo de proyectos informáticos con más de 5 años de experiencia en el área de Innovación y Desarrollo.
 
 Nuestros conocimientos y experiencias con las tecnologías actuales del mercado, son el pilar fundamental de nuestra organización.
 </p>
+            <input class="view-team btn btn--primary btn--large" id="view-team" type="button" value="ver equipo">
             </div>
+
             <div id="feature-2" class="features-textblock animated fadeIn">
                 <p>Misión</p>
                 <p> Apasionados por abordar las problemáticas de nuestros clientes, buscamos entregar la mejor solución acorde a su presupuesto.
@@ -61,21 +63,44 @@ Nuestra lema “Ingeniería de software para Soluciones Empresariales Integradas
             <div id="feature-3" class="features-textblock animated fadeIn">
                 <p>Visión</p>
                 <p> La tecnología en la actualidad es una herramienta imprescindible para las empresas e instituciones que buscan crecer y producir de forma eficaz. Buscamos ser un referente en el mercado nacional en áreas de las tecnologías de información, por esto es que hemos decidido ser una empresa en constante innovación, capaz de solucionar cualquier problemática que el sector de la tecnología requiera.
-</p>
+                </p>
             </div>
+             
         </div>
-        <div class="features-content-col">
+
+        <div id="figure-block" class="features-content-col">
             <div data-features-nav="" class="features-graph">
-                <div class="button-holder"><a href="#feature-1" class="icon-features-1 btn-with-icon __active"></a><a href="#feature-2" class="icon-features-2 btn-with-icon"></a><a href="#feature-3" class="icon-features-3 btn-with-icon"></a></div>
+                    <div class="button-holder" id="text-top">
+                        <a href="#feature-1" class="fas fa-users btn-with-icon __active">
+                            <span class="sq-bt-label label-top-left">Nosotros</span>
+                        </a>
+                        <a href="#feature-2" class="fas fa-bullseye btn-with-icon">
+                            <span class="sq-bt-label label-top">Misión</span>
+                        </a>
+                        <a href="#feature-3" class="far fa-eye btn-with-icon">
+                            <span class="sq-bt-label label-top-right">Visión</span>
+                        </a>
+                    </div>
                 <div class="animation-holder">
                     <span class="flash-oval">
-                <img src="http://127.0.0.1:8080/wp-content/themes/emblr-official/images/logo.svg" alt="pulse">
-                </span>
+                        <img src="http://127.0.0.1:8080/wp-content/themes/emblr-official/images/logo.svg" alt="pulse">
+                        <div class="wave hidden wave-anim"></div>
+                           <div class="wave2 hidden wave-anim"></div>
+                           <div class="wave3 hidden wave-anim"></div>
+                    </span>
                 </div>
+
                 <div class="button-holder">
-                    <a href="#feature-4" class="icon-features-4 btn-with-icon">
-                        </a><a href="#feature-5" class="icon-features-5 btn-with-icon"></a>
-                    <a href="#feature-6" class="icon-features-6 btn-with-icon"></a></div>
+                    <a href="#feature-4" class="fas fa-quote-left btn-with-icon">
+                        <span class="sq-bt-label label-bottom-left">Fundación</span>
+                    </a>
+                    <a href="#feature-5" class="fas fa-check btn-with-icon">
+                        <span class="sq-bt-label label-bottom">Features</span>
+                    </a>
+                    <a href="#feature-6" class="far fa-registered btn-with-icon">
+                        <span class="sq-bt-label label-bottom-right">Derecho</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
