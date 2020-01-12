2738ec73950dde79b13c505d55ea13bda3804142
Diego Ulloa <diegoulloao@icloud.com>
2020-01-12 19:13:46 -0300

    Entradas: se agrega header + menu completo

--- wordpress/wp-content/themes/emblr-official/single-post.php
+++ wordpress/wp-content/themes/emblr-official/single-post.php
@@ -8,7 +8,40 @@
          *  Objeto referencia al post actual
          * 
          */
-        global $post;
+        global
+            $post,
+            $wp_query
+        ;
+
+
+        /*
+        *   Título menú
+        */
+        $titulo_menu = get_field( 'titulo_menu', 'options' );
+
+
+        /*
+        *   Widgets menú
+        */
+        $widgets_menu = get_field( 'widgets_menu', 'options' );
+
+
+        /*
+        *   Redes menú
+        */
+        $redes_menu = get_field( 'redes_menu', 'options' );
+
+
+        /*
+        *   Mini logo esquinero
+        */
+        $logo_esquina = get_field( 'logo_esquina', 'options' );
+
+
+        /*
+        *   Búsqueda
+        */
+        $busqueda = get_field( 'busqueda', 'options' );
 
 
         /**
@@ -43,6 +76,96 @@
         $etiquetas = get_the_tags( $post-> ID );
 
     ?>
+
+
+    <!-- Header
+    ================================================== -->
+    <header class="s-header">
+        
+        <?  if ( $logo_esquina ) :  ?>
+
+        <div class="header-logo">
+            <a class="site-logo" href="#">
+
+                <img src="<?= get_template_directory_uri() ?>/images/logo.svg" alt="Homepage">
+            </a>
+        </div> <!-- end header-logo -->
+
+        <?  endif  ?>
+
+
+        <nav class="header-nav">
+            <div class="header-nav__bg"> <!-- blur background efx --> </div>
+
+            <div class="header-nav__content-container">
+                <a href="#0" class="header-nav__close" title="close"><span>Close</span></a>
+
+                <h3> <?= $titulo_menu ?> </h3>
+
+                <div class="header-nav__content">
+                    
+                    <?
+
+                        if ( function_exists( 'custom_wp_nav_menu' ) )
+                            custom_wp_nav_menu( 'header-nav__list' );
+
+                    ?>
+
+
+                    <?  if ( $widgets_menu ) :  ?>
+
+                    <div id="menu-widgets">
+
+                        <?  dynamic_sidebar( 'main-menu' )  ?>
+
+                    </div>
+
+                    <?  endif  ?>
+
+                </div> <!-- end header-nav__content -->
+            </div>
+
+        </nav> <!-- end header-nav -->
+
+        
+        <?  if ( $busqueda ) :  ?>
+
+        <div class="opaque-controls"></div>
+
+        <a class="icon-search">
+
+            <form role="search" method="get" id="searchform" action="<?= home_url( '/' ) ?>" class="search">
+
+                <input id="search" type="checkbox"/><label class="search-init" for="search"></label>
+                <label class="search-active" for="search"></label>
+
+                <div class="search-border"></div>
+
+                <!-- WORDPRESS SEARCH INPUT -->
+                <input
+                    type="text"
+                    id="s" name="s"
+                    class="search-field"
+                    placeholder="Buscar"
+                    value="<?= $_GET['s']?>"
+                    autofocus
+                />
+                <!-- /WORDPRESS SEARCH INPUT -->
+
+                <div class="close-search"></div>
+                
+            </form> <!-- search -->
+
+        </a>  <!-- icon-search -->
+
+        <?  endif  ?>
+
+
+        <a class="header-menu-toggle" href="#0">
+            <span class="header-menu-icon"></span>
+        </a>
+
+    </header> <!-- end s-header -->
 
 
     <!-- Contenido de entradas -->
@@ -256,7 +379,7 @@
 
     <!-- Contenido de entradas -->
 
-    
+
     <script type="text/javascript">
 
         ($ => {
