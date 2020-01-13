4e65160469456b0deb5a5e133f29e2f13283b922
Diego Ulloa <diegoulloao@icloud.com>
2020-01-13 06:37:05 -0300

    Se lleva top-header (.s-header) a archivo separado en directorio nuevo: template-parts | Se ajustan vistas main-page, search.php y single-post.php para importar template part de top-header

--- wordpress/wp-content/themes/emblr-official/search.php
+++ wordpress/wp-content/themes/emblr-official/search.php
@@ -26,40 +26,8 @@
     *   Objeto post (resultados)
     *
     */
-    global
-        $post,
-        $wp_query
-    ;
+    global $post;
 
-
-    /*
-    *   Título menú
-    */
-    $titulo_menu = get_field( 'titulo_menu', 'options' );
-
-
-    /*
-    *   Widgets menú
-    */
-    $widgets_menu = get_field( 'widgets_menu', 'options' );
-
-
-    /*
-    *   Redes menú
-    */
-    $redes_menu = get_field( 'redes_menu', 'options' );
-
-
-    /*
-    *   Mini logo esquinero
-    */
-    $logo_esquina = get_field( 'logo_esquina', 'options' );
-
-
-    /*
-    *   Búsqueda
-    */
-    $busqueda = get_field( 'busqueda', 'options' );
 
 ?>
 
@@ -68,147 +36,28 @@
 
 <body class="<? emblr_theme() ?>">
 
-    <!-- Header
-    ================================================== -->
-    <header class="s-header">
-        
-        <?  if ( $logo_esquina ) :  ?>
+    <!-- top-header: corner-logo - search and menu controls -->
 
-        <div class="header-logo">
-            <a class="site-logo" href="#">
+    <? set_query_var( 'focused_search', true ) ?>
+    <? set_query_var( 'posts_number_results', true ) ?>
 
-            <?
-            
-                if ( $wp_query->found_posts < 10 ) :
-                    $total_resultados = '0' . $wp_query->found_posts;
+    <? get_template_part( 'template-parts/top-header' ) ?>
 
-                else :
-                    $total_resultados = $wp_query->found_posts;
-
-                endif;
-
-            ?>
-
-                <img src="<?= get_template_directory_uri() ?>/images/logo.svg" alt="Homepage">
-                <span class="marca"> total <?= $total_resultados ?> </span>
-            </a>
-        </div> <!-- end header-logo -->
-
-        <?  endif  ?>
-
-
-        <nav class="header-nav">
-            <div class="header-nav__bg"> <!-- blur background efx --> </div>
-
-            <div class="header-nav__content-container">
-                <a href="#0" class="header-nav__close" title="close"><span>Close</span></a>
-
-                <h3> <?= $titulo_menu ?> </h3>
-
-                <div class="header-nav__content">
-                    
-                    <?
-
-                        if ( function_exists( 'custom_wp_nav_menu' ) )
-                            custom_wp_nav_menu( 'header-nav__list' );
-
-                    ?>
-
-
-                    <?  if ( $widgets_menu ) :  ?>
-
-                    <div id="menu-widgets">
-
-                        <?  dynamic_sidebar( 'main-menu' )  ?>
-
-                    </div>
-
-                    <?  endif  ?>
-
-
-
-                    <?  if ( $redes_menu ):  ?>
-
-                    <ul class="header-nav__social">
-
-                        <?  global $social_links  ?>
-
-                        <?  foreach ( $social_links as $social_name => $social ) :  ?>
-
-                        <?  if ( $social ) :
-
-                                switch ( $social_name ) :
-                                    case "facebook": $social_name .= "-f";      break;
-                                    case "linkedin": $social_name .= "-in";     break;
-                                endswitch
-                        ?>
-
-                        <li>
-                            <a href="<?= $social['url'] ?>" target="<?= $social['target'] ?>">
-                                <i class="fab fa-<?= $social_name ?>"></i>
-                            </a>
-                        </li>
-                        
-                        <?  endif  ?>
-                        <?  endforeach  ?>
-
-                    </ul>
-
-                    <?  endif  ?>
-
-                </div> <!-- end header-nav__content -->
-            </div>
-
-        </nav> <!-- end header-nav -->
-
-        
-        <?  if ( $busqueda ) :  ?>
-
-        <div class="opaque-controls"></div>
-
-        <a class="icon-search">
-
-            <form role="search" method="get" id="searchform" action="<?= home_url( '/' ) ?>" class="search">
-
-                <input id="search" type="checkbox" checked/><label class="search-init" for="search"></label>
-                <label class="search-active" for="search"></label>
-
-                <div class="search-border focus"></div>
-
-                <!-- WORDPRESS SEARCH INPUT -->
-                <input
-                    type="text"
-                    id="s" name="s"
-                    class="search-field"
-                    placeholder="Buscar"
-                    value="<?= $_GET['s']?>"
-                    autofocus
-                />
-                <!-- /WORDPRESS SEARCH INPUT -->
-
-                <div class="close-search"></div>
-                
-            </form> <!-- search -->
-
-        </a>  <!-- icon-search -->
-
-        <?  endif  ?>
-
-
-        <a class="header-menu-toggle" href="#0">
-            <span class="header-menu-icon"></span>
-        </a>
-
-    </header> <!-- end s-header -->
+    <!-- top-header: corner-logo - search and menu controls -->
 
 
     <!-- Sección: Resultado búsqueda -->
     <section id="search-results" class="section-page s-search-results" data-aos="fade-up">
         
-        <h1> Resultados de búsqueda: </h1>
-        <!-- <i class="fas fa-search"></i> --> <tiny> Usted ingresó: "<?= $_GET['s'] ?>" </tiny>
+        <h1> <?= __( 'Resultados de búsqueda', 'emblr' ) ?>: </h1>
+        <!-- <i class="fas fa-search"></i> -->
+        <tiny> <?= __( 'Usted ingresó', 'emblr' ) ?>: "<?= $_GET['s'] ?>" </tiny>
 
-        <? while ( have_posts() ) : the_post() ?>
+        <? while ( have_posts() ) :
+        
+            the_post()
+        ?>
+
             <article>
 
                 <a href="<? the_permalink() ?>">
--- wordpress/wp-content/themes/emblr-official/single-post.php
+++ wordpress/wp-content/themes/emblr-official/single-post.php
@@ -26,40 +26,8 @@
      *  Objeto referencia al post actual
      * 
      */
-    global
-        $post,
-        $wp_query
-    ;
+    global $post;
 
-
-    /*
-    *   Título menú
-    */
-    $titulo_menu = get_field( 'titulo_menu', 'options' );
-
-
-    /*
-    *   Widgets menú
-    */
-    $widgets_menu = get_field( 'widgets_menu', 'options' );
-
-
-    /*
-    *   Redes menú
-    */
-    $redes_menu = get_field( 'redes_menu', 'options' );
-
-
-    /*
-    *   Mini logo esquinero
-    */
-    $logo_esquina = get_field( 'logo_esquina', 'options' );
-
-
-    /*
-    *   Búsqueda
-    */
-    $busqueda = get_field( 'busqueda', 'options' );
 
 
     /**
@@ -100,126 +68,12 @@
 
 <body id="posts" class="<? emblr_theme() ?>">
 
-    <!-- Header
-    ================================================== -->
-    <header class="s-header">
-        
-        <?  if ( $logo_esquina ) :  ?>
+    <!-- top-header: corner-logo - search and menu controls -->
 
-        <div class="header-logo">
-            <a class="site-logo" href="#">
+    <? get_template_part( 'template-parts/top-header' ) ?>
 
-                <img src="<?= get_template_directory_uri() ?>/images/logo.svg" alt="Homepage">
-            </a>
-        </div> <!-- end header-logo -->
-
-        <?  endif  ?>
-
-
-        <nav class="header-nav">
-            <div class="header-nav__bg"> <!-- blur background efx --> </div>
-
-            <div class="header-nav__content-container">
-                <a href="#0" class="header-nav__close" title="close"><span>Close</span></a>
-
-                <h3> <?= $titulo_menu ?> </h3>
-
-                <div class="header-nav__content">
-                    
-                    <?
-
-                        if ( function_exists( 'custom_wp_nav_menu' ) )
-                            custom_wp_nav_menu( 'header-nav__list' );
-
-                    ?>
-
-
-                    <?  if ( $widgets_menu ) :  ?>
-
-                    <div id="menu-widgets">
-
-                        <?  dynamic_sidebar( 'main-menu' )  ?>
-
-                    </div>
-
-                    <?  endif  ?>
-
-
-
-                    <?  if ( $redes_menu ):  ?>
-
-                    <ul class="header-nav__social">
-
-                        <?  global $social_links  ?>
-
-                        <?  foreach ( $social_links as $social_name => $social ) :  ?>
-
-                        <?  if ( $social ) :
-
-                                switch ( $social_name ) :
-                                    case "facebook": $social_name .= "-f";      break;
-                                    case "linkedin": $social_name .= "-in";     break;
-                                endswitch
-                        ?>
-
-                        <li>
-                            <a href="<?= $social['url'] ?>" target="<?= $social['target'] ?>">
-                                <i class="fab fa-<?= $social_name ?>"></i>
-                            </a>
-                        </li>
-                        
-                        <?  endif  ?>
-                        <?  endforeach  ?>
-
-                    </ul>
-
-                    <?  endif  ?>
-
-                </div> <!-- end header-nav__content -->
-            </div>
-
-        </nav> <!-- end header-nav -->
-
-        
-        <?  if ( $busqueda ) :  ?>
-
-        <div class="opaque-controls"></div>
-
-        <a class="icon-search">
-
-            <form role="search" method="get" id="searchform" action="<?= home_url( '/' ) ?>" class="search">
-
-                <input id="search" type="checkbox"/><label class="search-init" for="search"></label>
-                <label class="search-active" for="search"></label>
-
-                <div class="search-border"></div>
-
-                <!-- WORDPRESS SEARCH INPUT -->
-                <input
-                    type="text"
-                    id="s" name="s"
-                    class="search-field"
-                    placeholder="Buscar"
-                    value="<?= $_GET['s']?>"
-                    autofocus
-                />
-                <!-- /WORDPRESS SEARCH INPUT -->
-
-                <div class="close-search"></div>
-                
-            </form> <!-- search -->
-
-        </a>  <!-- icon-search -->
-
-        <?  endif  ?>
-
-
-        <a class="header-menu-toggle" href="#0">
-            <span class="header-menu-icon"></span>
-        </a>
-
-    </header> <!-- end s-header -->
-
+    <!-- top-header: corner-logo - search and menu controls -->
+    
 
     <!-- Contenido de entradas -->
 
