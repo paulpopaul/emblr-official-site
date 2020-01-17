378571909610c8a5af2591564e77df098dcfc56a
Diego Ulloa <diegoulloao@icloud.com>
2020-01-17 15:48:58 -0300

    Reparacion de errores JS

--- wordpress/wp-content/themes/emblr-official/js/all.js
+++ wordpress/wp-content/themes/emblr-official/js/all.js
@@ -7,13 +7,13 @@
 
     "use strict";
     
-    var cfg = {
+    window.cfg = {
         scrollDuration : 800, // smoothscroll duration
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc',   // mailchimp url
         formSendURL    : '/wp-content/themes/emblr-official/actions/enviar-contacto.php'  // contact send url
     },
 
-    $WIN = $(window);
+    window.$WIN = $(window);
 
     // Add the User Agent to the <html>
     // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
