0a7711d30284a9f9c1d881b97a21525142a21055
Alvaro <whitesnake@MacBook-Pro-de-Alvaro.local>
2020-01-22 11:57:23 -0300

    Se estiliza el texto de subscripción a correos electrónicos, también se cambia el texto por español y otras frases

---
+++ wordpress/wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,4 @@
+<?php exit; ?>
+[2020-01-22 10:56:28] ERROR: Form 422 > Mailchimp API error: 400 Bad Request. Invalid Resource. asda**@ho*****.com looks fake or invalid, please enter a real email address.
+Request: POST https://us4.api.mailchimp.com/3.0/lists/c545dce6d5/members - {"status":"pending","email_address":"asda**@ho*****.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"172.21.0.1","tags":[]}
+Response: 400 Bad Request - {"type":"http://developer.mailchimp.com/documentation/mailchimp/guides/error-glossary/","title":"Invalid Resource","status":400,"detail":"asda**@ho*****.com looks fake or invalid, please enter a real email address.","instance":"b65033b8-5047-4a5d-bea5-dbe96f82340f"}
